375df1dce48c5f8230f5218fe206f572e07632a1
pzunedad <pedro.zunedadiego@gmail.com>
2025-01-31 18:04:59 +0100

    cambio4

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ import { GraphQLError } from "graphql";
 
 const MONGO_URL = Deno.env.get("MONGO_URL")
 if(!MONGO_URL){
-  console.error("Error MONGO_URL");
+  throw new GraphQLError("ERROR MONGO_URL")
 }
 
 const client = new MongoClient(MONGO_URL)
